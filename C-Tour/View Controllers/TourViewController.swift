56a47cec6e1d62657058c2c208e1535a0df99b27
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-04-17 21:07:08 -0400

    New commit

---
+++ C-Tour/View Controllers/TourViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TourViewController.swift
+//  C-Tour
+//
+//  Created by Fanuel Dana on 4/17/23.
+//
+
+import UIKit
+
+class TourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

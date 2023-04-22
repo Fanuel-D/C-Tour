8bd0e4e9873ca9ed657f6682f8cb042d131f0f5f
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-04-22 13:35:22 -0400

    Tour Page Done!

--- C-Tour/View Controllers/CorrectViewController.swift
+++ C-Tour/View Controllers/CorrectViewController.swift
@@ -14,20 +14,27 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         picture.image = UIImage(named: "Parrish")
         mapy.delegate = self
+        wiser.text = "Parrish"
+        texter.text = "Parrish Hall, the College’s iconic oldest building, sits in the center of campus and serves as a hub of campus life, with Admissions, the Dean’s Office, the Post Office, and the President’s Office on the first two floors and residence hall rooms above. "
+        
+        
+        
         let location = locations[0]
         let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.0025, longitudeDelta: 0.0025)
+        let span = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
         annotation.title = location.imageName
         mapy.setRegion(region, animated: true)
         mapy.addAnnotation(annotation)
-    
+        
+        
         // Do any additional setup after loading the view.
     }
-    
    
+   
+    @IBOutlet weak var wiser: UILabel!
     let locations = [
         Location(imageName: "Parrish", latitude: 39.905379084102805, longitude: -75.35420943232845),
         Location(imageName: "SciCenter", latitude: 39.906792551164735, longitude: -75.35571378999889),
@@ -39,14 +46,7 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
     ]
     
     
-    func getCoordinates(from locations: [Location]) -> [CLLocationCoordinate2D] {
-        var coordinates = [CLLocationCoordinate2D]()
-        for location in locations {
-            let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-            coordinates.append(coordinate)
-        }
-        return coordinates
-    }
+    
     
     
     @IBOutlet weak var mapy: MKMapView!
@@ -57,12 +57,13 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
     var tapCount = 1
     @IBAction func tap(_ sender: UIButton) {
         if tapCount == 1 {
-            texter.text = "Button tapped once"
+            texter.text = "A new 145,000-SF facility joined all of Swarthmore’s science departments including physics, astronomy, mathematics/statistics, chemistry, biology, computer science and the science library under one roof. "
+            wiser.text = "Science Center"
             picture.image = UIImage(named: "SciCenter")
             
                 let location = locations[1]
                 let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-                let span = MKCoordinateSpan(latitudeDelta: 0.0025, longitudeDelta: 0.0025)
+                let span = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
                 let region = MKCoordinateRegion(center: coordinate, span: span)
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = coordinate
@@ -72,7 +73,8 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
             
             
         } else if tapCount == 2 {
-            texter.text = "Button tapped twice"
+            texter.text = "McCabe Library is the center of the College Libraries system. The Circulation, Technical Services and Reference Departments occupy the main floor. Other facilities include study carrels, lounges, computers for student use, conference rooms, and faculty studies. "
+            wiser.text = "Mccabe Library"
             picture.image = UIImage(named: "Mccabe")
             
             let location = locations[2]
@@ -85,7 +87,8 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
             mapy.setRegion(region, animated: true)
             mapy.addAnnotation(annotation)
         } else if tapCount == 3 {
-            texter.text = "Button tapped 3"
+            texter.text = "As the new home of Swarthmore’s engineering, biology, and psychology departments, Singer Hall affirms the college’s interdisciplinary values and supports their long-term strategic plan."
+            wiser.text = "Singer Hall"
             picture.image = UIImage(named: "Singer")
             
             let location = locations[3]
@@ -98,8 +101,9 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
             mapy.setRegion(region, animated: true)
             mapy.addAnnotation(annotation)
         } else if tapCount == 4 {
-            texter.text = "Button tapped 4 times"
+            texter.text = "Wharton, centrally located and with beautiful views of the Crum and the amphitheater, is divided into three sections (AB, CD, and EF), CD houses only seniors. AB and EF house first-year student. "
             picture.image = UIImage(named: "Wharton")
+            wiser.text = "Wharton"
             
             let location = locations[4]
             let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
@@ -111,8 +115,9 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
             mapy.setRegion(region, animated: true)
             mapy.addAnnotation(annotation)
         } else if tapCount == 5 {
-            texter.text = "Button tapped 5 times"
+            texter.text = "Built in 1906, Beardsley Hall was created using an architectural cutting-edge material technology: molded concrete blocks. Today, this unique structure is the home to an 1,100 sq ft painting studio.  In addition to the Art Office, you can also find faculty and staff’s offices."
             picture.image = UIImage(named: "Beardsley")
+            wiser.text = "Beardsley"
             
             let location = locations[5]
             let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
@@ -124,8 +129,9 @@ class CorrectViewController: UIViewController, MKMapViewDelegate {
             mapy.setRegion(region, animated: true)
             mapy.addAnnotation(annotation)
         } else if tapCount == 6 {
-            texter.text = "Button tapped 6 times"
+            texter.text = "Dining in community is at the heart of the College experience. Gathering to share a meal brings a chance to rest, share stories, and build lasting relationships with friends and colleagues. "
             picture.image = UIImage(named: "DCC")
+            wiser.text = "DCC"
             
             let location = locations[6]
             let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
